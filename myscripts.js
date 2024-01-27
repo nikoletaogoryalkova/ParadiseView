cc2a68caeb9b105f579f12dcbec3acf4c55f734d
Nikoleta Ogoryalkova <35707730+nikoletaogoryalkova@users.noreply.github.com>
2024-01-27 17:24:22 +0200

    Add files via upload

--- myscripts.js
+++ myscripts.js
@@ -1,11 +1,13 @@
 let burgerMenu = document.getElementById('burger-menu');
 let overlay = document.getElementById('nav');
-burgerMenu.addEventListener('click',function(){
+burgerMenu.addEventListener('click', function () {
   this.classList.toggle("close");
   overlay.classList.toggle("overlay");
 });
 
 
-$('.selected').on('click', function(){
-	$('.language span').show('slow');	
+$('.selected').on('click', function () {
+  $('.language span').show('slow');
 });
+
+// Back to top
